ade638518e49f5f22e8d544b3ee704382214b187
Dmitry Zelenetskiy <dmitry.zelenetskiy@gmail.com>
2017-01-15 21:07:56 +0200

    updating

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,6 +14,11 @@ const entries = [
 
 // PLUGINS
 const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  }),
   new ExtractTextPlugin("app.css"),
   new webpack.NamedModulesPlugin(),
   new webpack.LoaderOptionsPlugin({
